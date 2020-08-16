528e5b0fc2702242eadf316ded85274a821e694c
savestatecorrupted <68752965+savestatecorrupted@users.noreply.github.com>
2020-08-16 04:42:24 -0500

    Movement and jump
    Made base movement and began working on jump functionality.
    
    Moving backwards/forwards has proper animations

---
+++ PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    //necessary for animations and physics
+    private Rigidbody2D rb2D;
+    private Animator myAnimator;
+
+    private bool facingRight = true;
+
+    //variables to play with
+    public float speed = 2.0f;
+    public float horizMovement;
+
+
+    private void Start()
+    {
+        //define player components
+        rb2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    //handles input for physics
+
+    private void Update()
+    {
+        //check direction player inputted
+        horizMovement = Input.GetAxisRaw("Horizontal");
+
+
+    }
+
+    //handles running physics
+
+    private void FixedUpdate()
+    {
+        //move player left or right
+        rb2D.velocity = new Vector2(horizMovement * speed, rb2D.velocity.y);
+        myAnimator.SetFloat("Speed",horizMovement);
+        
+    }
+
+}
